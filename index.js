366cd9981735dbe125628c967c511915814b6162
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-06 09:19:16 +0600

    added assignments reading api

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    // Send a ping to confirm a successful connection
+
+     const assignmentCollection = client.db('onlineStudy').collection('assignments');
+
+    
+    app.get('/assignments', async(req, res) => {
+      const cursor = assignmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
