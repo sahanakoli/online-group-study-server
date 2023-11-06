e5bca8fc23539ec72b053d6dc155bb02b9d17203
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-06 20:52:49 +0600

    post an assignment to the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,12 +29,28 @@ async function run() {
 
      const assignmentCollection = client.db('onlineStudy').collection('assignments');
 
+    app.post('/assignments', async(req, res) =>{
+      const newAssignment = req.body;
+      console.log(newAssignment);
+      const result = await assignmentCollection.insertOne(newAssignment);
+      res.send(result);
+    })
+
     
     app.get('/assignments', async(req, res) => {
       const cursor = assignmentCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.get("/assignments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id),};
+      const result = await assignmentCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
      // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
